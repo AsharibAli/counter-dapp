c975a756d6d182984b2447c5a603fb48ae407a30
Asharib Ali <asharibarain4@gmail.com>
2024-05-07 22:58:06 +0500

    fix the issue

--- front_end/next.config.js
+++ front_end/next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-  exportTrailingSlash: true,
-  assetPrefix: './',
+  trailingSlash: true,
+  // assetPrefix: './',
 }
